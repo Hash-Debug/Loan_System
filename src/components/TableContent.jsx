b03abb9f6f6693f38bb275bce841aec19883117c
ani <amazinganirudhhere@gmail.com>
2023-01-28 16:08:08 +0530

    loan table update

--- src/components/TableContent.jsx
+++ src/components/TableContent.jsx
@@ -7,44 +7,58 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { AccessTime, Clear, Done, } from '@mui/icons-material';
-import { Chip } from '@mui/material';
+import { Chip, CircularProgress } from '@mui/material';
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { db } from '../api/firebase';
 
 
 
 
 function TableContent() {
+
+    const [loan, setloan] = React.useState(null)
+    // const [docid, setdocid] = React.useState(null)
+
     React.useEffect(() => {
         async function fetchUserData() {
-    
-    
-          const q = await query(collection(db, "users"), where("userdata.uid", "==", window.localStorage.getItem('uid')));
-          // console.log(q);
-    
-          const querySnapshot = await getDocs(q);
-          console.log(querySnapshot);
-          querySnapshot.forEach((doc) => {
-            // here we are getting the user data and loan from it 
-            // doc.data() is never undefined for query doc snapshots
-            console.log(doc.id, " => ", doc.data());
-            setuser(doc.data().Loans) 
-            setdocid(doc.id)
-          });
+
+
+            const q = await query(collection(db, "users"), where("userdata.uid", "==", window.localStorage.getItem('uid')));
+            // console.log(q);
+
+            const querySnapshot = await getDocs(q);
+            console.log(querySnapshot);
+            querySnapshot.forEach((doc) => {
+                // here we are getting the user data and loan from it 
+                // doc.data() is never undefined for query doc snapshots
+                console.log(doc.id, " => ", doc.data().Loan);
+                setloan(doc.data().Loan)
+                // setdocid(doc.id)
+            });
         }
         fetchUserData()
-      }, [])
+    }, [])
 
-    const rows = [
-        createData('Frozen yoghurt', 159, 6.0, 24, <Chip label="Approved" color='success' icon={<Done />} />),
-        createData('Cupcake', 305, 3.7, 67, <Chip label="Pending" color='warning' icon={<AccessTime />} />),
-        createData('Eclair', 262, 16.0, 24, <Chip label="Approved" color='success' icon={<Done />} />),
-        createData('Cupca', 305, 3.7, 67, <Chip label="Pending" color='warning' icon={<AccessTime />} />),
-        createData('Ice cream sandwich', 237, 9.0, 37, <Chip label="Approved" color='success' icon={<Done />} />),
-        createData('Gingerbread', 356, 16.0, 49, <Chip label="Rejected" color='error' icon={<Clear />} />),
-    ];
-
-    function createData(name, amount, duration, interest, status) {
-        return { name, amount, duration, interest, status };
+    const status= {
+        1: <Chip label="Approved" color='success' icon={<Done />} />,
+        2: <Chip label="Pending" color='warning' icon={<AccessTime />} />,
+        3: <Chip label="Rejected" color='error' icon={<Clear />} />
     }
+
+
+
+    // const rows = [
+    //     createData('Frozen yoghurt', 159, 6.0, 24, <Chip label="Approved" color='success' icon={<Done />} />),
+    //     createData('Cupcake', 305, 3.7, 67, <Chip label="Pending" color='warning' icon={<AccessTime />} />),
+    //     createData('Eclair', 262, 16.0, 24, <Chip label="Approved" color='success' icon={<Done />} />),
+    //     createData('Cupca', 305, 3.7, 67, <Chip label="Pending" color='warning' icon={<AccessTime />} />),
+    //     createData('Ice cream sandwich', 237, 9.0, 37, <Chip label="Approved" color='success' icon={<Done />} />),
+    //     createData('Gingerbread', 356, 16.0, 49, <Chip label="Rejected" color='error' icon={<Clear />} />),
+    // ];
+
+    // function createData(name, amount, duration, interest, status) {
+    //     return { name, amount, duration, interest, status };
+    // }
 
 
 
@@ -53,7 +67,7 @@ function TableContent() {
             <Table sx={{ minWidth: 650, borderRadius: '20px' }} aria-label="simple table">
                 <TableHead>
                     <TableRow>
-                        <TableCell>Loan Name</TableCell>
+                        <TableCell>Loan Type</TableCell>
                         <TableCell align="right">Amount</TableCell>
                         <TableCell align="right">Duration</TableCell>
                         <TableCell align="right">Interest</TableCell>
@@ -61,20 +75,27 @@ function TableContent() {
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {rows.map((row) => (
+                    {loan ? loan.map((row,key) => (
                         <TableRow
-                            key={row.name}
+                            key={key}
                             sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                         >
                             <TableCell component="th" scope="row">
-                                {row.name}
+                                {row.LoanType}
                             </TableCell>
-                            <TableCell align="right">{row.amount}</TableCell>
-                            <TableCell align="right">{row.duration}</TableCell>
+                            <TableCell align="right">{row.LoanAmnt}</TableCell>
+                            <TableCell align="right">{row.Duration}</TableCell>
                             <TableCell align="right">{row.interest}</TableCell>
-                            <TableCell align="right">{row.status}</TableCell>
+                            <TableCell align="right">{status[1]}</TableCell>
                         </TableRow>
-                    ))}
+                    )) : <TableRow>
+                        <TableCell>
+                            <CircularProgress />
+
+                        </TableCell>
+
+                    </TableRow>
+                    }
                 </TableBody>
             </Table>
         </TableContainer>
